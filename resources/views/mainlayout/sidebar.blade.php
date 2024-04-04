ca9a674b1503cc5c15d9791a99fee436940a1cb7
Fathir Akmal Burhanudin <starboyaja@gmail.com>
2024-04-04 12:32:30 +0700

    feat: User Interface update for main dashboardd

--- resources/views/mainlayout/sidebar.blade.php
+++ resources/views/mainlayout/sidebar.blade.php
@@ -197,7 +197,7 @@ class="z-10 max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 my-4 ml-4 block w-fu
         @if (request()->is('books*'))
         <li class="mt-0.5 w-full">
             <a class="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors"
-                href="/book">
+                href="/books">
                 <div
                     class="bg-gradient-to-tl from-purple-700 to-pink-500 shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5">
                     <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1"
@@ -229,7 +229,7 @@ class="z-10 max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 my-4 ml-4 block w-fu
         @else
         <li class="mt-0.5 w-full">
             <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors"
-                href="/book">
+                href="/books">
                 <div
                     class="shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5">
                     <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1"
