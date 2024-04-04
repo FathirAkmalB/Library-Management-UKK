1bc4a9e2c9a1b56a36cec68d5dff2ccc403522b1
Fathir Akmal Burhanudin <starboyaja@gmail.com>
2024-04-04 12:27:06 +0700

    feat: User Interface update

--- resources/views/Auth/login_user.blade.php
+++ resources/views/Auth/login_user.blade.php
@@ -1,89 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
+    <title>WiseBook</title>
+    <!-- Fonts and icons -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Nucleo Icons -->
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Main Styling -->
+    <link href="../assets/css/soft-ui-dashboard-tailwind.css?v=1.0.5" rel="stylesheet" />
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WiseBook | Login</title>
+    <!-- Nepcha Analytics (nepcha.com) -->
+    <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+    <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+  </head>
 
-    <!-- Font Awesome -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
-
-    <!-- Google Font Ubuntu -->
-    <link href="" rel="stylesheet">
-
-    <link rel="stylesheet" href="{{ asset('assets/css/particle.css') }}">
-
-
-    <script src="https://cdn.tailwindcss.com"></script>
-
-</head>
-
-<style>
-    *{
-        overflow: hidden;
-    }
-</style>
-
-<body>
-    <div id="stars"></div>
-    <div id="stars2"></div>
-    
-    <section class="w-full bg-gray-800 flex justify-center items-center h-screen">
-        <div class="w-5/6 bg-gray-800 bg-opacity-95 rounded-2xl shadow-md dark:border md:mt-0 sm:max-w-md xl:p-0">
-            <div class="w-full p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 class="text-xl md:text-2xl font-bold leading-tight tracking-tight text-gray-900  dark:text-white">
-                    Sign in to WiseBook account
-                </h1>
-                @if (session('status'))
-                    <div>
-                        <p class="text-red-600">{{ session('status') }} {{ session('message') }}</p>
-                    </div>
-                @endif
-                <form class="space-y-4 md:space-y-6" action="{{ route('authentication_user') }}">
-                    <div>
-                        <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username<span class="text-red-400">*</span></label>
-                        <input type="text" name="username" id="username"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600  dark:text-white focus:outline-1 outline-gray-700"
-                            placeholder="enter username" required>
-                    </div>
-                    <div>
-                        <label for="password"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password<span class="text-red-400">*</span></label>
-                        <input type="password" name="password" id="password" placeholder="••••••••"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 focus:outline-1 outline-gray-700  dark:text-white "
-                            required>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-start">
-                            <div class="flex items-center h-5">
-                                <input id="remember" aria-describedby="remember" type="checkbox"
-                                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 ">
-                            </div>
-                            <div class="ml-3 text-sm">
-                                <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                            </div>
-                        </div>
-                        <a href="#"
-                            class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot
-                            password?</a>
-                    </div>
-                    <button type="submit"
-                        class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign
-                        in</button>
-                    <div class="flex justify-center">
-                        <p class="text-sm font-light text-gray-50">
-                            Don’t have an account yet? <a href="{{ route('register_user') }}"
-                                class="font-medium text-blue-600 hover:underline dark:text-blue-500">Sign up</a>
-                        </p>
-                    </div>
-                </form>
+  <body class="m-0 font-sans antialiased font-normal bg-white text-start text-base leading-default text-slate-500">
+   
+    <main class="mt-0 transition-all duration-200 ease-soft-in-out">
+      <section>
+        <div class="relative flex items-center p-0 overflow-hidden bg-center bg-cover min-h-75-screen">
+          <div class="container z-10">
+            <div class="flex flex-wrap mt-0 -mx-3">
+              <div class="flex flex-col w-full max-w-full px-3 mx-auto md:flex-0 shrink-0 md:w-6/12 lg:w-5/12 xl:w-4/12">
+                <div class="relative flex flex-col min-w-0 mt-32 break-words bg-transparent border-0 shadow-none rounded-2xl bg-clip-border">
+                  <div class="p-6 pb-0 mb-0 bg-transparent border-b-0 rounded-t-2xl">
+                    <h3 class="relative z-10 font-bold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">Welcome back</h3>
+                    <p class="mb-0">Enter your username and password to sign in</p>
+                  </div>
+                  <div class="flex-auto p-6" >
+                      <form role="form" method="POST" action="{{ route('authentication_user') }}">
+                        @csrf
+                      <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Username</label>
+                      <div class="mb-4">
+                        <input type="username" class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow" placeholder="username" aria-label="username" aria-describedby="username-addon" name="username"/>
+                      </div>
+                      <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Password</label>
+                      <div class="mb-4">
+                        <input type="password" class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow" placeholder="Password" aria-label="Password" aria-describedby="password-addon" name="password"/>
+                      </div>
+                      <div class="min-h-6 mb-0.5 block pl-12">
+                        <input id="rememberMe" class="mt-0.54 rounded-10 duration-250 ease-soft-in-out after:rounded-circle after:shadow-soft-2xl after:duration-250 checked:after:translate-x-5.25 h-5 relative float-left -ml-12 w-10 cursor-pointer appearance-none border border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] checked:border-slate-800/95 checked:bg-slate-800/95 checked:bg-none checked:bg-right" type="checkbox" checked="" />
+                        <label class="mb-2 ml-1 font-normal cursor-pointer select-none text-sm text-slate-700" for="rememberMe">Remember me</label>
+                      </div>
+                      <div class="text-center">
+                        <button type="submit" class="inline-block w-full px-6 py-3 mt-6 mb-0 font-bold text-center text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer shadow-soft-md bg-x-25 bg-150 leading-pro text-xs ease-soft-in tracking-tight-soft bg-gradient-to-tl from-blue-600 to-cyan-400 hover:scale-102 hover:shadow-soft-xs active:opacity-85">Sign in</button>
+                      </div>
+                    </form>
+                  </div>
+                  <div class="p-6 px-1 pt-0 text-center bg-transparent border-t-0 border-t-solid rounded-b-2xl lg:px-2">
+                    <p class="mx-auto mb-6 leading-normal text-sm">
+                      Don't have an account?
+                      <a href="../pages/sign-up.html" class="relative z-10 font-semibold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">Sign up</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="w-full max-w-full px-3 lg:flex-0 shrink-0 md:w-6/12">
+                <div class="absolute top-0 hidden w-3/5 h-full -mr-32 overflow-hidden -skew-x-10 -right-40 rounded-bl-xl md:block">
+                  <div class="absolute inset-x-0 top-0 z-0 h-full -ml-16 bg-cover skew-x-10" style="background-image: url('../assets/img/curved-images/curved6.jpg')"></div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </section>
-
-</body>
-
+      </section>
+    </main>
+    
+  </body>
+  <!-- plugin for scrollbar  -->
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js" async></script>
+  <!-- main script file  -->
+  <script src="../assets/js/soft-ui-dashboard-tailwind.js?v=1.0.5" async></script>
 </html>
