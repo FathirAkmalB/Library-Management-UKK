db321ff2254cce6145e2a400b3537c6b1b276de6
Fathir Akmal Burhanudin <starboyaja@gmail.com>
2024-04-04 12:35:13 +0700

    fix: user interface booklend

--- database/migrations/2024_03_03_141903_create_books_table.php
+++ database/migrations/2024_03_03_141903_create_books_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('book_code');
             $table->string('title');
+            $table->string('cover');
             $table->string('status')->default('in stock');
             $table->timestamps();
         });
